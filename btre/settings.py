a602862f8594f8b4f9445a095b60ec4d6225e685
PETER MENNETTI <69663513+petemennetti@users.noreply.github.com>
2023-06-15 09:41:10 -0400

    contacts logic

--- btre/settings.py
+++ btre/settings.py
@@ -139,3 +139,12 @@ MESSAGE_TAGS = {
     messages.ERROR: "danger" 
      
 }
+
+
+# Email config
+
+EMAIL_HOST  = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'peter.mennetti@gmail.com'
+EMAIL_HOST_PASSWORD = '6144254812'
+EMAIL_USE_TLS = True

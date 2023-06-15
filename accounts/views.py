e12215ea347de5ac1aeb26d65fc598e99474a415
PETER MENNETTI <69663513+petemennetti@users.noreply.github.com>
2023-06-15 10:07:29 -0400

    DONE

--- accounts/views.py
+++ accounts/views.py
@@ -1,31 +1,30 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
-# from contacts.models import Contact
+from contacts.models import Contact
 
 def register(request):
-  
   if request.method == 'POST':
-    #Get form values
-    first_name = request.POST['first_name'] 
-    last_name = request.POST['last_name'] 
-    username = request.POST['username'] 
-    email = request.POST['email'] 
-    password = request.POST['password'] 
-    password2 = request.POST['password2'] 
-    
-    #check if password match
-    if password==password2:
-      #Check user name
+    # Get form values
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+    username = request.POST['username']
+    email = request.POST['email']
+    password = request.POST['password']
+    password2 = request.POST['password2']
+
+    # Check if passwords match
+    if password == password2:
+      # Check username
       if User.objects.filter(username=username).exists():
-        messages.error(request,'That username is already in the database.')
-        return redirect( 'register') 
+        messages.error(request, 'That username is taken')
+        return redirect('register')
       else:
-        #check email
         if User.objects.filter(email=email).exists():
-          messages.error(request,'That email is already in the database.')
-          return redirect( 'register')  
+          messages.error(request, 'That email is being used')
+          return redirect('register')
         else:
+          # Looks good
           user = User.objects.create_user(username=username, password=password,email=email, first_name=first_name, last_name=last_name)
           # Login after register
           # auth.login(request, user)
@@ -35,12 +34,11 @@ def register(request):
           messages.success(request, 'You are now registered and can log in')
           return redirect('login')
     else:
-      messages.error(request,'Passwords do not match')
-      return redirect( 'register')
+      messages.error(request, 'Passwords do not match')
+      return redirect('register')
   else:
     return render(request, 'accounts/register.html')
- 
- 
+
 def login(request):
   if request.method == 'POST':
     username = request.POST['username']
@@ -64,13 +62,10 @@ def logout(request):
     messages.success(request, 'You are now logged out')
     return redirect('index')
 
-
-
 def dashboard(request):
-  
-  # user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
+  user_contacts = Contact.objects.order_by('-contact_date').filter(user_id=request.user.id)
 
-  # context = {
-  #   'contacts': user_contacts
-  # }
-  return render(request, 'accounts/dashboard.html', {})
+  context = {
+    'contacts': user_contacts
+  }
+  return render(request, 'accounts/dashboard.html', context)
